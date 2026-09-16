1c4a75dff357327c53ed8c0cc43211e12eb42cab
XoDeR <xoder.vs@gmail.com>
2026-09-16 19:07:18 +0200

    createtripp is called in main

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
+	"ride-sharing/services/trip-service/internal/domain"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
+	ctx := context.Background()
+
 	inmemRepo := repository.NewInmemRepository()
 
 	svc := service.NewService(inmemRepo)
 
-	_ = svc
+	fare := &domain.RideFareModel{
+		ID:     primitive.NewObjectID(),
+		UserID: "12",
+	}
+	svc.CreateTrip(ctx, fare)
+	//_ = svc
 }
