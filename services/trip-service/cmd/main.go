cc26df96b377e834f9e22b18ba0b42a4485a52e8
XoDeR <xoder.vs@gmail.com>
2026-09-16 18:59:11 +0200

    init repo and svc in main

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"ride-sharing/services/trip-service/internal/infrastructure/repository"
+	"ride-sharing/services/trip-service/internal/service"
+)
+
+func main() {
+	inmemRepo := repository.NewInmemRepository()
+
+	svc := service.NewService(inmemRepo)
+
+	_ = svc
+}
