abbd40bd61cf72be2a673a1745047e0298cb1a15
XoDeR <xoder.vs@gmail.com>
2026-09-17 16:11:29 +0200

    feat: http server implemented

---
+++ services/api-gateway/http.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"ride-sharing/shared/contracts"
+)
+
+func handleTripPreview(w http.ResponseWriter, r *http.Request) {
+	var reqBody previewTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close() // body will be closed, this is harmless but redundant
+
+	// validation
+	if reqBody.UserID == "" {
+		http.Error(w, "user ID is required", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: call trip service
+
+	response := contracts.APIResponse{Data: "ok"}
+
+	writeJSON(w, http.StatusCreated, response)
+
+	// TODO: for test, remove later
+	log.Println("SUCCESS")
+
+}
--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -14,10 +14,17 @@ var (
 func main() {
 	log.Println("Starting API Gateway")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello from API Gateway"))
-	})
+	// mux is http server multiplexer, actually it is a router
+	mux := http.NewServeMux()
 
-	http.ListenAndServe(httpAddr, nil)
+	mux.HandleFunc("POST /trip/preview", handleTripPreview)
+
+	server := &http.Server{
+		Addr:    httpAddr,
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("HTTP server error: %v", err)
+	}
 }
