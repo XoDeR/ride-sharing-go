b45344e7a28f40ac421120c9b77b377c13870186
XoDeR <xoder.vs@gmail.com>
2026-09-17 16:36:42 +0200

    feat: communicate to trip service via http : complete

--- services/trip-service/internal/infrastructure/http/http.go
+++ services/trip-service/internal/infrastructure/http/http.go
@@ -1,24 +1,49 @@
 package http
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"ride-sharing/services/trip-service/internal/domain"
+	"ride-sharing/shared/types"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type HttpHandler struct {
 	Service domain.TripService
 }
 
-func (s *HttpHandler) HandleTripPreview(w http.ResponseWriter, r *http.Request) {
-
+type previewTripRequest struct {
+	UserID      string           `json:"userID"`
+	Pickup      types.Coordinate `json:"pickup"`
+	Destination types.Coordinate `json:"destination"`
 }
 
-// fare := &domain.RideFareModel{
-// 		ID:     primitive.NewObjectID(),
-// 		UserID: "12",
-// 	}
+func (s *HttpHandler) HandleTripPreview(w http.ResponseWriter, r *http.Request) {
+	var reqBody previewTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
 
-// t, err := svc.CreateTrip(ctx, fare)
-// 	if err != nil {
-// 		log.Println(err)
-// 	}
+	fare := &domain.RideFareModel{
+		ID:     primitive.NewObjectID(),
+		UserID: "12",
+	}
+
+	ctx := r.Context()
+
+	t, err := s.Service.CreateTrip(ctx, fare)
+	if err != nil {
+		log.Println(err)
+	}
+
+	writeJSON(w, http.StatusOK, t)
+}
+
+func writeJSON(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
