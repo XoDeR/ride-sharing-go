fec6e12d91064a3571dfd5226b1d938b0f800585
XoDeR <xoder.vs@gmail.com>
2026-09-22 21:28:11 +0200

    feat: driver service finished

--- services/driver-service/grpc_handler.go
+++ services/driver-service/grpc_handler.go
@@ -24,13 +24,22 @@ func NewGrpcHandler(s *grpc.Server, service *Service) {
 }
 
 func (h *driverGrpcHandler) RegisterDriver(ctx context.Context, req *pb.RegisterDriverRequest) (*pb.RegisterDriverResponse, error) {
-	// TODO call the service method
+	driver, err := h.service.RegisterDriver(req.GetDriverID(), req.GetPackageSlug())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to register driver")
+	}
 
-	return nil, status.Errorf(codes.Unimplemented, "method RegisterDriver not implemented")
+	return &pb.RegisterDriverResponse{
+		Driver: driver,
+	}, nil
 }
 
 func (h *driverGrpcHandler) UnregisterDriver(ctx context.Context, req *pb.RegisterDriverRequest) (*pb.RegisterDriverResponse, error) {
-	// TODO call the service method
+	h.service.UnregisterDriver(req.GetDriverID())
 
-	return nil, status.Errorf(codes.Unimplemented, "method UnregisterDriver not implemented")
+	return &pb.RegisterDriverResponse{
+		Driver: &pb.Driver{
+			Id: req.GetDriverID(),
+		},
+	}, nil
 }
